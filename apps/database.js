a891cdc2762fc02eea3941c90f676769cc7ab99c
Yongki Kim <ygflove@likelion.org>
2023-08-05 22:33:16 +0900

    :white_check_mark: 변수 조정

--- apps/database.js
+++ apps/database.js
@@ -4,9 +4,8 @@ const logger = require("../utils/logger");
 const config = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: "wanted_pre_onboarding",
 };
 
 class Database {
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("./apps/database");
 
 const app = require("./apps")();
 const logger = require("./utils/logger");
-const port = process.env.PORT;
+const port = 3000;
 
 app.listen(port, () => {
   logger.info(`API 서버가 ${port}포트에서 활성화되었습니다.`);
