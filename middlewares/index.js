4ccbd1d3fadd28db5443056cda64a2fd29ad7910
Yongki Kim <ygflove@likelion.org>
2023-08-13 16:01:44 +0900

    :white_check_mark: 유효한 게시글 일련번호 형식 처리

--- middlewares/index.js
+++ middlewares/index.js
@@ -28,6 +28,17 @@ function validateUserParam(req, res, next) {
   return next();
 }
 
+function validatePostSeq(req, res, next) {
+  const { postSeq } = req.params;
+
+  if (!Number(postSeq)) {
+    res.status(statusCodes.BAD_REQUEST);
+    return res.json({ message: "유효하지 않은 게시글 일련번호 형식입니다." });
+  }
+
+  return next();
+}
+
 function verifyToken(req, res, next) {
   if (!req.headers.authorization) {
     res.status(statusCodes.UNAUTHORIZED);
@@ -64,6 +75,7 @@ function handleErrorModule(err, req, res) {
 
 module.exports = Object.freeze({
   validateUserParam,
+  validatePostSeq,
   verifyToken,
   handleErrorEndpoint,
   handleErrorModule,
--- routes/post-route.js
+++ routes/post-route.js
@@ -1,4 +1,9 @@
-module.exports = ({ postController: controller, express, verifyToken }) => {
+module.exports = ({
+  postController: controller,
+  express,
+  validatePostSeq,
+  verifyToken,
+}) => {
   const router = express.Router();
 
   // 과제 3. 새로운 게시글을 생성하는 엔드포인트
@@ -8,13 +13,18 @@ module.exports = ({ postController: controller, express, verifyToken }) => {
   router.get("/list", controller.getPostList);
 
   // 과제 5. 특정 게시글을 조회하는 엔드포인트
-  router.get("/:postSeq", controller.getPost);
+  router.get("/:postSeq", validatePostSeq, controller.getPost);
 
   // 과제 6. 특정 게시글을 수정하는 엔드포인트
-  router.put("/:postSeq", verifyToken, controller.putPost);
+  router.put("/:postSeq", validatePostSeq, verifyToken, controller.putPost);
 
   // 과제 7. 특정 게시글을 삭제하는 엔드포인트
-  router.delete("/:postSeq", verifyToken, controller.deletePost);
+  router.delete(
+    "/:postSeq",
+    validatePostSeq,
+    verifyToken,
+    controller.deletePost
+  );
 
   return router;
 };
